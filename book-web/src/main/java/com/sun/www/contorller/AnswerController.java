ddf1b2d3986ba9343ed8eba63bd344631e1dd6ae
sy <sy@timevale.com>
2017-09-15 18:04:49 +0800

    add

--- book-web/src/main/java/com/sun/www/contorller/AnswerController.java
+++ book-web/src/main/java/com/sun/www/contorller/AnswerController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -17,9 +19,28 @@ import java.util.List;
 @Controller
 public class AnswerController {
 
-    @RequestMapping(value = {"/index.html","/"}, method = RequestMethod.GET)
-    public String book(ModelMap modelMap) {
-        modelMap.addAttribute("title","小菊花 - 首页");
+    @RequestMapping(value = {"/index.html", "/"}, method = RequestMethod.GET)
+    public String index(ModelMap modelMap, HttpServletRequest request) {
+        String sessionIp = request.getSession().getAttribute("ip") == null ? "" : request.getSession().getAttribute("ip").toString();
+        String ip = request.getRemoteHost();
+        if (ip.equals(sessionIp)) {
+            return "second";
+        }
+        modelMap.addAttribute("title", "小菊花 - 首页");
         return "index";
+    }
+
+    @RequestMapping(value = "/second.html", method = RequestMethod.GET)
+    public String second(ModelMap modelMap, HttpServletRequest request) {
+        modelMap.addAttribute("title", "小菊花 - 测试");
+        return "second";
+    }
+
+    @RequestMapping(value = "/identity.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Object test(ModelMap modelMap, HttpServletRequest request, String name, String mobile) {
+        String ip = request.getRemoteHost();
+        request.getSession().setAttribute("ip", ip);
+        return "second";
     }
 }
