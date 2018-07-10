f4a4eb85d0c33a47eea8f152d42a17699848473c
sy <sy@timevale.com>
2018-07-10 17:34:45 +0800

    test

---
+++ book-dao/src/main/java/com/sun/www/bean/Account.java
@@ -0,0 +1,75 @@
+package com.sun.www.bean;
+
+import java.util.Date;
+
+public class Account {
+    private String id;
+
+    private String accountId;
+
+    private String name;
+
+    private String licenseNumber;
+
+    private Integer licenseType;
+
+    private Integer status;
+
+    private Date createDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId == null ? null : accountId.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber == null ? null : licenseNumber.trim();
+    }
+
+    public Integer getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(Integer licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
